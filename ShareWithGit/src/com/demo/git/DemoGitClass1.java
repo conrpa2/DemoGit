985687316316d238e93a742befeb6a847c0f0808
conrpa2 <ritesh.pareek@hotmail.com>
2013-06-25 11:11:58 +0200

    Pushing first-time to Remote Repo from Local -> Remote

---
+++ ShareWithGit/src/com/demo/git/DemoGitClass1.java
@@ -0,0 +1,17 @@
+package com.demo.git;
+
+public class DemoGitClass1 {
+
+	public DemoGitClass1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hi, This is first commit commets");
+
+	}
+
+}
